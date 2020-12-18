117984e84ee69f660e805a337994a04ff6a5bac5
fiachmchh <fiachmchh@gmail.com>
2020-12-18 19:30:00 +0000

    updated app and changed some js to jquery

--- weather.js
+++ weather.js
@@ -1,29 +1,30 @@
 var button = document.querySelector('.button');
 var inputValue = document.querySelector('.inputValue');
-var name = document.querySelector('.name');
-var date = document.querySelector('.date');
-var icon = document.querySelector('.icon');
+// var name = document.querySelector('.name');
+// var date = document.querySelector('.date');
+// var icon = document.querySelector('.icon');
 
+$(".button").on("click", function() {
 
-button.addEventListener('click', function(){
-    fetch('http://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=781431958b474e0ff56bb530277f295b')
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    var nameValue = data.city.name
-    var dateValue = data['list']['0']['dt_txt'];
-    var iconValue = data['list']['0']['weather']['0'];
-    var iconcode = iconValue.icon
+    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=781431958b474e0ff56bb530277f295b';
 
-    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    console.log(iconurl)
-    console.log(dateValue)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
 
-    // name.innerHTML = nameValue;
-    // date.innerHTML = dateValue;
-    // icon.innerHTML = iconValue;
+    .then(function(data) {
+        var nameValue = data.city.name
+        var dateValue = data['list']['0']['dt_txt'];
+        var iconValue = data['list']['0']['weather']['0'];
+        var iconcode = iconValue.icon
 
-    var img = $('<img>')
+        console.log(nameValue)
+        console.log(dateValue)
+
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+        var img = $('<img>')
 
     img.attr('src', iconurl);
 
@@ -34,15 +35,15 @@ button.addEventListener('click', function(){
     var text =$('<p>')
         text.text(nameValue + ' ' + dateValue);
         
-
     // dressthem how u want
 
     // 3 punch stick on page
     console.log('ABOUT OT STICK ON PAGE')
     containerDiv.append(text, img)
     $('.future').append(containerDiv)
+        
+    })
 })
 
 
-//.catch(err => alert("wrong city name"))
-})
+
