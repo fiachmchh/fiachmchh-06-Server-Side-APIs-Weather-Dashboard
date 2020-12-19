3517fceb20fa24fe3fb3ade203a1ab431e4403e2
fiachmchh <fiachmchh@gmail.com>
2020-12-19 20:25:12 +0000

    weather app carrying out required function, minus styling and some code polishing

--- weather.js
+++ weather.js
@@ -46,7 +46,7 @@ $(".button").on("click", function() {
     // dressthem how u want
 
     // 3 punch stick on page
-    console.log('ABOUT OT STICK ON PAGE')
+    // console.log('ABOUT OT STICK ON PAGE')
     containerDiv.append(img, text, temp, date, humi, wind)
     
     $('.current').append(containerDiv)
@@ -89,11 +89,6 @@ $(".5daybutton").on("click", function() {
 
     var humi =$('<p>')
     humi.text(humValue);
-   
-    
-
- 
-        
         
     // dressthem how u want
 
@@ -105,3 +100,145 @@ $(".5daybutton").on("click", function() {
         
     })
 })
+
+$(".5daybutton").on("click", function() {
+
+    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=metric&appid=781431958b474e0ff56bb530277f295b';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(data) {
+        var dateValue = data.list[2].dt_txt;
+        var iconValue = data.list[2].weather[0];
+        var iconcode = iconValue.icon
+        var tempValue = data.list[2].main.temp
+        var humValue = data.list[2].main.humidity
+        
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+        var img = $('<img>')
+
+    img.attr('src', iconurl);
+
+    // console.log('are we good ???',nameValue, dateValue, iconValue, tempValue)
+
+
+    var containerDiv = $('<div>');
+    
+    var temp =$('<p>')
+    temp.text(tempValue);
+    
+    var date =$('<p>')
+    date.text(dateValue);
+
+    var humi =$('<p>')
+    humi.text(humValue);
+        
+    // dressthem how u want
+
+    // 3 punch stick on page
+    console.log('ABOUT OT STICK ON PAGE')
+    containerDiv.append(img, temp, date, humi)
+    
+    $('.day3').append(containerDiv)
+        
+    })
+})
+
+$(".5daybutton").on("click", function() {
+
+    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=metric&appid=781431958b474e0ff56bb530277f295b';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(data) {
+        var dateValue = data.list[3].dt_txt;
+        var iconValue = data.list[3].weather[0];
+        var iconcode = iconValue.icon
+        var tempValue = data.list[3].main.temp
+        var humValue = data.list[3].main.humidity
+        
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+        var img = $('<img>')
+
+    img.attr('src', iconurl);
+
+    // console.log('are we good ???',nameValue, dateValue, iconValue, tempValue)
+
+
+    var containerDiv = $('<div>');
+    
+    var temp =$('<p>')
+    temp.text(tempValue);
+    
+    var date =$('<p>')
+    date.text(dateValue);
+
+    var humi =$('<p>')
+    humi.text(humValue);
+        
+    // dressthem how u want
+
+    // 3 punch stick on page
+    console.log('ABOUT OT STICK ON PAGE')
+    containerDiv.append(img, temp, date, humi)
+    
+    $('.day4').append(containerDiv)
+        
+    })
+})
+
+$(".5daybutton").on("click", function() {
+
+    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=metric&appid=781431958b474e0ff56bb530277f295b';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(data) {
+        var dateValue = data.list[4].dt_txt;
+        var iconValue = data.list[4].weather[0];
+        var iconcode = iconValue.icon
+        var tempValue = data.list[4].main.temp
+        var humValue = data.list[4].main.humidity
+        
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+        var img = $('<img>')
+
+    img.attr('src', iconurl);
+
+    // console.log('are we good ???',nameValue, dateValue, iconValue, tempValue)
+
+
+    var containerDiv = $('<div>');
+    
+    var temp =$('<p>')
+    temp.text(tempValue);
+    
+    var date =$('<p>')
+    date.text(dateValue);
+
+    var humi =$('<p>')
+    humi.text(humValue);
+        
+    // dressthem how u want
+
+    // 3 punch stick on page
+    console.log('ABOUT OT STICK ON PAGE')
+    containerDiv.append(img, temp, date, humi)
+    
+    $('.day5').append(containerDiv)
+        
+    })
+})
+
