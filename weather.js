98896b881e744d2c1c7295f8e8dd350e473ac6cd
fiachmchh <fiachmchh@gmail.com>
2020-12-22 14:45:00 +0000

    names of searched cities appearing at side

--- weather.js
+++ weather.js
@@ -15,6 +15,33 @@ $(".button").on("click", function() {
 
     .then(function(data) {
         var nameValue = data.city.name
+
+    var containerDiv = $('<div>');
+    var text =$('<p>')
+        text.text(nameValue);
+    
+   
+    // dressthem how u want
+
+    // 3 punch stick on page
+    // console.log('ABOUT OT STICK ON PAGE')
+    containerDiv.append(text)
+    $('.places').append(containerDiv)
+        
+    })
+})
+
+$(".button").on("click", function() {
+
+    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=metric&appid=781431958b474e0ff56bb530277f295b';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(data) {
+        var nameValue = data.city.name
         var dateValue = data.list[0].dt_txt;
         var iconValue = data.list[0].weather[0];
         var iconcode = iconValue.icon
